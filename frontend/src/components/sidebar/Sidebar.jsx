e3db2a7ea7dfcf29c83920afa16d47cad437b614
walker2u <kumar99prasad@gmail.com>
2024-10-29 22:15:28 +0530

    created basic home component and side bar for it.

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import SearchInput from './SearchInput.jsx'
 import Conversations from './Conversations.jsx'
+import LogoutButton from './LogoutButton.jsx'
 
 function Sidebar() {
     return (
-        <div>
+        <div className='border-r border-slate-500 p-4 flex flex-col'>
             <SearchInput />
+            <div className='divider px-3'></div>
             <Conversations />
+            <LogoutButton />
         </div>
     )
 }
